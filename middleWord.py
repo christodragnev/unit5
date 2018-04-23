865fc004f11fd1e846e3ebf06fd044f664315696
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-04-23 08:38:12 -0400

    Updated from Brython Server: 4/23/2018 8:38:12 AM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,11 @@
+#Christo Dragnev
+#4/23/18
+#middleWord.py
+
+words = input('Type in some words: ').split(' ')
+
+if len(words)%2==0:
+    print(words[len(words)/2-1],words[len(words)/2])
+else:
+    print(words[len(words)/2])
 
