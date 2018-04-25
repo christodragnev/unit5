d85d7e417ee8b126fb3bb3ddaa1a5868a7cfd988
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-04-25 08:18:37 -0400

    Updated from Brython Server: 4/25/2018 8:18:37 AM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,18 @@
+#Christo Dragnev
+#4/25/18
+#warmup13.py - making a list of 20 random numbers between 1 and 100
 
+from random import randint
+
+myList = []
+
+i=0
+while i<=20:
+    myList.append(randint(1,100))
+    i+=1
+print(myList)
+
+print(sum(myList))
+print(max(myList))
+print(min(myList))
+    
