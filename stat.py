dec7930a43b3bcd56b0074212d4d08e7824e982d
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-04-26 08:49:27 -0400

    Updated from Brython Server: 4/26/2018 8:49:27 AM

--- stat.py
+++ stat.py
@@ -1,1 +1,12 @@
+#Christo Dragnev
+#4/26/18
+#stat.py
 
+numbers = input('Type a list of numbers: ').split(' ')
+
+for num in numbers:
+    print(num)
+
+print('Min: ',min(numbers))
+print('Max: ',max(numbers))
+print('Mean: ', sum(numbers)/len(numbers))
