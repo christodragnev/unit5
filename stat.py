cdf133e68363625ce66a8fd2f14acd174ab7b4d5
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-04-27 08:49:59 -0400

    Updated from Brython Server: 4/27/2018 8:49:58 AM

--- stat.py
+++ stat.py
@@ -2,11 +2,21 @@
 #4/26/18
 #stat.py
 
-numbers = input('Type a list of numbers: ').split(' ')
+L = []
+while True:
+    number = input('Type a list of numbers: ')
+    if number!='q':
+        L.append(float(number))
+    else:
+        break
+   
 
-for num in numbers:
+for num in L:
     print(num)
 
-print('Min: ',min(numbers))
-print('Max: ',max(numbers))
-print('Mean: ', sum(numbers)/len(numbers))
+print('Min: ',min(L))
+print('Max: ',max(L))
+print('Mean: ', sum(L)/(len(L)))
+
+L.count(item)
+print('Median:',
