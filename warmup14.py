2f98185398f27b9b336f16e72aa1670e0e16ff29
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-04-30 08:25:41 -0400

    Updated from Brython Server: 4/30/2018 8:25:41 AM

--- warmup14.py
+++ warmup14.py
@@ -1,1 +1,10 @@
+#Christo Dragnev
+#4/30/18
+#warmup14.py
 
+#asking using for first and last name
+#tell them what their initials are
+
+words = input('Enter some words: ').split(' ')
+print('Your initials are ',words[0][0]+'.',words[1][0]+'.')
+    
