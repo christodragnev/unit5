4fe747bd1a9422fb155946cb7aad7ed68066501a
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-04 08:33:39 -0400

    Updated from Brython Server: 5/4/2018 8:33:37 AM

--- warmup15.py
+++ warmup15.py
@@ -1,1 +1,13 @@
+#Christo Dragnev
+#5/4/18
+#warmup15.py function that takes a list of numbers as an argument and returns a new list with each number doubled
+
+
+def double(L):
+    L = []
+    
+    
+    
+print(double([1,2,3]))
+    
 
