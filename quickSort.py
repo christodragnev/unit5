e629dd47ea6064d892fdf3150996e9b3e8d2d597
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-04 08:39:49 -0400

    Updated from Brython Server: 5/4/2018 8:39:48 AM

--- quickSort.py
+++ quickSort.py
@@ -5,28 +5,24 @@
 from random import randint
 from time import time
 
-N = 10 #how many numbers will be sorted
+N = 1000 #how many numbers will be sorted
 
 def mySort(A, lo, hi):
     if lo<hi:
         p = partition(A, lo, hi)
-        mySort(A,lo,p)
+        mySort(A,lo,p-1)
         mySort(A,p+1,hi)
-    print(A)
     return A
 
 def partition(A, lo, hi):
-    pivot=A[lo]
+    pivot=A[hi]
     i = lo-1
-    j= hi+1
-    while True:
-        while A[i] < pivot:
+    for j in range(lo,hi):
+        if A[j] < pivot:
             i = i + 1
-        while A[j] > pivot:
-            j = j-1
-        if i >= j:
-            return j
-        A[i], A[j] = A[j], A[i] #swap in python
+            A[i], A[j] = A[j], A[i]
+    A[i+1], A[hi] = A[hi], A[i+1]
+    return i+1
         
     
 if __name__ == '__main__':
