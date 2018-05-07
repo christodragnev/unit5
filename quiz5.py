b3a230318aac268dafe6839b67d77d2220a7ede7
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-07 08:30:58 -0400

    Updated from Brython Server: 5/7/2018 8:30:58 AM

--- quiz5.py
+++ quiz5.py
@@ -3,19 +3,19 @@
 #quiz5.py
 
 def penultimate(L1):
-    print(L1[-2])
+    return L1[-2]
     
 def plusEquals(a,b):
     new = []
     for item in a:
         new.append(item+b)
-    print(new)
+    return new
     
 def smallest(L2):
     L2.sort()
-    print(L2[0])
+    return L2[0]
     
     
-penultimate([3,4,5,6,7])
-plusEquals([1,2,3,4],10)
-smallest([1,2,3,4])
+print(penultimate([3,4,5,6,7]))
+print(plusEquals([1,2,3,4],10))
+print(smallest([1,2,3,4]))
