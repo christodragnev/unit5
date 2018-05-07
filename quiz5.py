a7fa1a4ed0a646d157e987fe883fd3d3f907e221
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-07 08:29:49 -0400

    Updated from Brython Server: 5/7/2018 8:29:48 AM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,21 @@
+#Christo Dragnev
+#5/7/18
+#quiz5.py
 
+def penultimate(L1):
+    print(L1[-2])
+    
+def plusEquals(a,b):
+    new = []
+    for item in a:
+        new.append(item+b)
+    print(new)
+    
+def smallest(L2):
+    L2.sort()
+    print(L2[0])
+    
+    
+penultimate([3,4,5,6,7])
+plusEquals([1,2,3,4],10)
+smallest([1,2,3,4])
